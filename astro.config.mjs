e3289818f80b433ef81c283e2fa13977b4205a0b
Adeosun Abiola <abiolaolalekan39@gmail.com>
2025-01-17 12:02:43 +0100

    changed the method by which images are imported

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), icon()]
+  site: "https://bmeson.netlify.app/",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
 });
